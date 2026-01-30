6318c1a901621fdad2e23770bb7e82bd7cb718a5
Jhunna Kumar <jhunnaroy@gmail.com>
2026-01-30 11:35:30 +0530

    Fix auth bug

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://assignment-todo-app-frontend1.onrender.com/",
   credentials: true
 }));
 app.use(express.json());
