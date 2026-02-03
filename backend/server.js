a5a169f87a91c754c3b4de16fd202c5558a11c69
Jhunna Kumar <jhunnaroy@gmail.com>
2026-02-03 23:38:29 +0530

    comit change

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      
       "https://assignment-todo-app-frontend1.onrender.com"
     ],
     credentials: true
